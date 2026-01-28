1696ea0dab8fbb57559b1c1aade4753bfa556e7a
Mogesh G <92145955+MogeshG@users.noreply.github.com>
2026-01-28 11:39:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,45 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+const button = document.querySelector(".submit");
+const result = document.querySelector(".result");
+const input = document.querySelector(".input")
+
+button.addEventListener("click", () => {
+  const values = input.value; 
+  if (!values) {
+    result.innerHTML = "Please enter some numbers!";
+    return;
+  }
+
+  const ropes = values.split(",").map(num => parseInt(num.trim()));
+
+  if (ropes.some(isNaN)) {
+    result.innerHTML = "Invalid input! Please enter numbers only.";
+    return;
+  }
+  const cost = minCost(ropes);
+  result.innerHTML = `Minimum cost to connect ropes: ${cost}`;
+});
+function minCost(arr) {
+  arr.sort((a, b) => a - b);
+
+  let cost = 0;
+
+  while (arr.length > 1) {
+    let first = arr.shift();
+    let second = arr.shift();
+
+    let sum = first + second;
+    cost += sum;
+    arr.push(sum);
+    arr.sort((a, b) => a - b); 
+  }
+
+  return cost;
 }
+
+console.log(minCost([4, 3, 2, 6])); 
+console.log(minCost([1, 2, 3, 4, 5]));
+
+
+
 
 module.exports=mincost;
